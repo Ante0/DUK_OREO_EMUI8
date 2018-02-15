029bac3ef153dd24d8f1a4e1178ba0b37f5da1ab
Andreas <ante00@gmail.com>
2018-02-15 13:35:12 +0100

    update scripts

--- pack_kernel.sh
+++ pack_kernel.sh
@@ -3,6 +3,7 @@ cp out/arch/arm64/boot/Image.gz kernel/tools/kernel
 cd tools/
 ./pack_kernerimage_cmd.sh
 mv kernel.img ../kernel.img
+rm kernel
 cd ../
 echo "kernel.img created in:"
 realpath kernel.img
